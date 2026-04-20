d03d1e11c5e84d9a8cdfdfe12a6c828d26b980a8
Ijidamir Thomas Wazamda <wazamskid@gmail.com>
2026-04-20 12:58:09 +0100

    fix import path again

--- src/components/RelatedProducts.jsx
+++ src/components/RelatedProducts.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import relatedF  from '../assets/relatedF.js'
+import related from '../assets/related.JS'
 import Item from './Item'
 
 const RelatedProducts = () => {
@@ -8,7 +8,7 @@ const RelatedProducts = () => {
       <h1 className='text-center font-bold text-xl sm:text-2xl my-5'>Related Products</h1>
       <hr className='w-full h-1 border bg-gray-900 '/>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-7 gap-2'>
-        {relatedF.map((item,i)=>(
+        {related.map((item,i)=>(
             <Item key={i}  id={item.id} name={item.name}  new_price={item.new_price} image={item.image} />
         ))}
       </div>
